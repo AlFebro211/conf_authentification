c18343057609270a031c86a4f45fe3c95b9ceca3
NdayiragijeAlainFabrice <alainfabbricendayiragije98@gmail.com>
2023-05-13 08:42:54 +0200

    verifier

--- info.py
+++ info.py
@@ -3,3 +3,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER ='alfebro211@gmail.com'
 EMAIL_HOST_PASSWORD = '0390511923'
 EMAIL_PORT=587
+
+
+
+# --------------------------------verification
+# --------veriiiiifoopppppp
+# ------veridfoooaaa;;;x
